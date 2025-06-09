47edfeaf87bc06189daca9f5b9e90202a8681a27
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-06-09 11:21:57 +0530

    feat: added project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router'
+import { AppRouter } from './components'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter/>
+  </BrowserRouter>
+  )
+}
+
+export default App
